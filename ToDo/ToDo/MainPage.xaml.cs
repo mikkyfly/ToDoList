e010886a6dd3491c4ff174f5cfd7c59e3eee1c16
moscowx1 <moscowx1@mail.ru>
2020-08-15 17:18:25 +0500

    Add project files.

---
+++ ToDo/ToDo/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ToDo
+{
+	public partial class MainPage : ContentPage
+	{
+		List<User> Users;
+
+		List<Task> Tasks;
+
+		public MainPage()
+		{
+			InitializeComponent();
+			Users = UserGenerator.GenerateUsers();
+			Tasks = TaskGenerator.GenerateTasks(Users);
+
+		}
+	}
+}
